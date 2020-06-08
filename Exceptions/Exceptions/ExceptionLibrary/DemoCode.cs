f5c4d0aed13d17aab7a420bfa7aa43ccfc9ca42f
Tim Janssens <timjanssens81@gmail.com>
2020-06-08 15:28:52 +0200

    Exceptions Intro Tim Corey

---
+++ Exceptions/Exceptions/ExceptionLibrary/DemoCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionLibrary
+{
+    public class DemoCode
+    {
+
+        public int GrandparentMethod(int position)
+        {
+            int output = 0;
+
+            Console.WriteLine("Open Database Connection");
+
+            try
+            {
+                output = ParentMethod(position);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("you gave us bad information. Bad user");
+            }
+            //catch (Exception ex)
+            //{
+            //    //do some logging
+            //    throw new ArgumentException("you passed in bad data", ex);
+            //}
+
+            finally
+            {
+                Console.WriteLine("Close Database Connection");
+            }
+
+
+            return output;
+        }
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            //try
+            //{
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+
+            //}
+
+            return output;
+        }
+
+    }
+}
---
+++ Exceptions/Exceptions/ExceptionsConsoleAppTimCorey/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExceptionLibrary;
+using System;
+
+namespace ExceptionsConsoleAppTimCorey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DemoCode demo = new DemoCode();
+
+
+
+            try
+            {
+                int result = demo.GrandparentMethod(4);
+                Console.WriteLine($"The value at the given position is {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
