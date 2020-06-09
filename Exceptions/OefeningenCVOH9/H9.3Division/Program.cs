ef67c36067324ecd25143b606c8e6ff8233fe9f0
Tim Janssens <timjanssens81@gmail.com>
2020-06-09 13:36:45 +0200

    Solve H9 divide

---
+++ Exceptions/OefeningenCVOH9/H9.3Division/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace H9._3Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Give a value for a: ");
+                int valueA = int.Parse(Console.ReadLine());
+
+                Console.Write("Give a value for b: ");
+                int valueB = int.Parse(Console.ReadLine());
+                int modulus = valueA % valueB;
+                Console.WriteLine($"The modulus of a divided by b is: {modulus}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
