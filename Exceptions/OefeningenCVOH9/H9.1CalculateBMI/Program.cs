092910f5a24ef0fb58a3608550af3e90ae16c29f
Tim Janssens <timjanssens81@gmail.com>
2020-06-09 13:17:05 +0200

    Solve H9 Calculate BMI and PrimeNumbers

---
+++ Exceptions/OefeningenCVOH9/H9.1CalculateBMI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace H9._1CalculateBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wat is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"{name}, we are going to calculate your BMI.");
+
+            try
+            {
+                Console.WriteLine($"{name}, how big are you (150cm is 1.5)?");
+                double lenght = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{name}, how much do you weight in kg?");
+                double weight = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{name}, your BMI is {CalculateBMI(lenght, weight)}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Wrong input");
+                
+            }
+
+
+
+            Console.ReadLine();
+        }
+
+        private static double CalculateBMI(double lenght, double weight) => weight / (lenght * lenght);
+
+    }
+}
---
+++ Exceptions/OefeningenCVOH9/H9.2PrimeNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace H9._2PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberTill = 0;
+            int primeCoutner = 0;
+
+            try
+            {
+                Console.WriteLine("Till witch number do you want to get prime numbers");
+                numberTill = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Not a valid number");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+            Console.WriteLine("Found Primes:");
+
+            for (int i = 0; i <= numberTill; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    Console.WriteLine(i);
+                    primeCoutner++;
+                }
+            }
+
+            Console.WriteLine($"Found {primeCoutner} primes");
+
+
+
+            Console.ReadKey();
+        }
+
+        private static bool CheckPrime(int num)
+        {
+            if (num > 1)
+            {
+
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                        return false;
+                }
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
