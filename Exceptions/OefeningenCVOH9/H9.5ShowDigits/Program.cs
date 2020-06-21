1b664ec5f3469f7117fe251f798e8f34dae56a76
Tim <timjanssens81@gmail.com>
2020-06-21 19:32:58 +0200

    Make layout KassaSystem Zoo

---
+++ Exceptions/OefeningenCVOH9/H9.5ShowDigits/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace H9._5ShowDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wich character do you want to use?");
+            char chosenChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            const int minCharLine = 10;
+            const int maxCharLine = 100;
+            const int minTime = 1;
+            const int maxTime = 1000;
+
+            int charTimesPerLine = ReadNumberWriteMessage($"How many times do you want this char on a line? The number must be bigger than {minCharLine} and smaller than {maxCharLine}", minCharLine, maxCharLine);
+            int timelimit = ReadNumberWriteMessage($"How many milliseconds do you want to pause between each line? The number must be bigger than {minTime} and smaller than {maxTime}", minTime, maxTime);
+
+
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// checks if the number is between the min and max value of the parameters
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int ReadNumberWriteMessage(string message, int min, int max)
+        {
+
+            Console.WriteLine(message);
+            int charTimesPerLine = ReadNumberWriteMessage($"That is not valid, please give a number: ");
+
+            while (true)
+            {
+                if (charTimesPerLine < min)
+                {
+                    Console.WriteLine($"please enter a number higher then {min}: ");
+                    charTimesPerLine = ReadNumberWriteMessage($"That is not valid, please give a number: ");
+                }
+                else if (charTimesPerLine > max)
+                {
+                    Console.WriteLine($"Please enter a number lower then {max}: ");
+                    charTimesPerLine = ReadNumberWriteMessage($"That is not valid, please give a number: ");
+                }
+                else
+                    break;
+            }
+
+
+            return charTimesPerLine;
+        }
+
+        /// <summary>
+        /// check if there is an int as input
+        /// </summary>
+        /// <param name="message">message when someone types in an invalid character</param>
+        /// <returns></returns>
+        public static int ReadNumberWriteMessage(string message)
+        {
+            while (true)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    return number;
+                }
+                catch (Exception)
+                {
+                    Console.Write(message);
+                }
+            }
+        }
+    }
+}
---
+++ KassasysteemZoo/KassasysteemZoo/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KassasysteemZoo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int BabyAge = 4;
+        const int ChildAge = 12;
+
+        int age = 0;
+        int babyCount = 0;
+        int childCount = 0;
+        int adultCount = 0;
+        int totalPersonsCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            age = int.Parse(txtLeeftijd.Text);
+            if (age < BabyAge)
+                babyCount++;
+            else if (age < ChildAge)
+                childCount++;
+            else
+                adultCount++;
+
+            totalPersonsCount++;
+
+            txtYoungerThan4.Text = babyCount.ToString();
+            txtYoungerThan12.Text = childCount.ToString();
+            txtOlderThan12.Text = adultCount.ToString();
+            txtTotalPersons.Text = totalPersonsCount.ToString();
+
+            txtLeeftijd.Focus();
+            txtLeeftijd.Text = string.Empty;
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnNewGroup_Click(object sender, RoutedEventArgs e)
+        {
+            txtLeeftijd.Focus();
+            txtLeeftijd.Text = string.Empty;
+            txtYoungerThan4.Text = string.Empty;
+            txtYoungerThan12.Text = string.Empty;
+            txtOlderThan12.Text = string.Empty;
+            txtTotalPersons.Text = string.Empty;
+            txtTotalBill.Text = string.Empty;
+
+            babyCount = 0;
+            childCount = 0;
+            adultCount = 0;
+            totalPersonsCount = 0;
+        }
+
+        private void enter(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
