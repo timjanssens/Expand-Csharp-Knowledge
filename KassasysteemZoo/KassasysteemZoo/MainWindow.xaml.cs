b0cae3d51bf8bb1a4b1d693cba76f6675173e5ce
Tim Janssens <timjanssens81@gmail.com>
2020-06-24 19:51:03 +0200

    Add function only valid numberinput

--- KassasysteemZoo/KassasysteemZoo/MainWindow.xaml.cs
+++ KassasysteemZoo/KassasysteemZoo/MainWindow.xaml.cs
@@ -20,41 +20,109 @@ namespace KassasysteemZoo
     /// </summary>
     public partial class MainWindow : Window
     {
+        //declaration constants
         const int BabyAge = 4;
         const int ChildAge = 12;
+        const int PriceChild = 12;
+        const int PriceAdult = 18;
 
-        int age = 0;
+        //declaration variables
+        byte age = 0;
         int babyCount = 0;
         int childCount = 0;
         int adultCount = 0;
         int totalPersonsCount = 0;
+        int fantaCount = 0;
+        int candyCount = 0;
+        int friesCount = 0;
+        double priceWholeGroup = 0;
+        double discount = 0;
+
+        
         public MainWindow()
         {
             InitializeComponent();
+            txtLeeftijd.Focus();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            age = int.Parse(txtLeeftijd.Text);
-            if (age < BabyAge)
-                babyCount++;
-            else if (age < ChildAge)
-                childCount++;
+            //check if the input is a byte
+            if (byte.TryParse(txtLeeftijd.Text, out age))
+            {
+                //divide persons in age groups
+                if (age < BabyAge)
+                    babyCount++;
+                else if (age < ChildAge)
+                    childCount++;
+                else
+                    adultCount++;
+                
+                //check wich gift is chosen
+                if (rdbFanta.IsChecked == true)
+                    fantaCount++;
+                else if (rdbCandy.IsChecked == true)
+                    candyCount++;
+                else
+                    friesCount++;
+
+                //add total counter
+                totalPersonsCount++;
+
+                //update amount of persons per group
+                txtYoungerThan4.Text = babyCount.ToString();
+                txtYoungerThan12.Text = childCount.ToString();
+                txtOlderThan12.Text = adultCount.ToString();
+                txtTotalPersons.Text = totalPersonsCount.ToString();
+
+                //update gifts per group
+                txtFanta.Text = fantaCount.ToString();
+                txtCandy.Text = candyCount.ToString();
+                txtFries.Text = friesCount.ToString();
+
+
+                //calculate the price for the whole group
+                priceWholeGroup = (childCount * PriceChild) + (adultCount * PriceAdult);
+
+                //calculate discount
+                if (childCount >= 5)
+                    discount = priceWholeGroup * 0.1;
+                if (adultCount >= 5)
+                    discount = priceWholeGroup * 0.2;
+
+                //subtract discount from price for the whole group
+                priceWholeGroup -= discount;
+
+                //update the pricePerGroupField and discount
+                txtTotalBill.Text = priceWholeGroup.ToString();
+                txtDiscount.Text = discount.ToString();
+
+                //put focus after input back on input field
+                txtLeeftijd.Focus();
+
+                //empty the input field
+                txtLeeftijd.Text = string.Empty;
+
+
+                    
+             
+
+             
+
+            }
             else
-                adultCount++;
+            {
+                MessageBox.Show("It must be a number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtLeeftijd.Focus();
+                txtLeeftijd.Text = string.Empty;
 
-            totalPersonsCount++;
-
-            txtYoungerThan4.Text = babyCount.ToString();
-            txtYoungerThan12.Text = childCount.ToString();
-            txtOlderThan12.Text = adultCount.ToString();
-            txtTotalPersons.Text = totalPersonsCount.ToString();
-
-            txtLeeftijd.Focus();
-            txtLeeftijd.Text = string.Empty;
-
+            }
         }
-
+        /// <summary>
+        /// close the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
@@ -62,23 +130,38 @@ namespace KassasysteemZoo
 
         private void btnNewGroup_Click(object sender, RoutedEventArgs e)
         {
+            //focus back on input field
             txtLeeftijd.Focus();
+
+            //empty all fields amount of persons per group
             txtLeeftijd.Text = string.Empty;
             txtYoungerThan4.Text = string.Empty;
             txtYoungerThan12.Text = string.Empty;
             txtOlderThan12.Text = string.Empty;
             txtTotalPersons.Text = string.Empty;
             txtTotalBill.Text = string.Empty;
+            txtDiscount.Text = string.Empty;
+            txtFanta.Text = string.Empty;
+            txtCandy.Text = string.Empty;
+            txtFries.Text = string.Empty;
+            
+       
 
+
+            //reset amount of persons per group
             babyCount = 0;
             childCount = 0;
             adultCount = 0;
             totalPersonsCount = 0;
+            priceWholeGroup = 0;
+            discount = 0;
+            fantaCount = 0;
+            candyCount = 0;
+            friesCount = 0;
         }
 
-        private void enter(object sender, KeyEventArgs e)
-        {
 
-        }
+
+
     }
 }
